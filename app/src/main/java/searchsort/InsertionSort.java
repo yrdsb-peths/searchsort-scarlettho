11cca5a353d46fb7630f8ed31b2e2561ccee4682
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-12-06 13:25:06 -0500

    test

--- app/src/main/java/searchsort/InsertionSort.java
+++ app/src/main/java/searchsort/InsertionSort.java
@@ -5,6 +5,18 @@ public class InsertionSort {
      * Sort the array arr using insertion sort algorithm.
      */
     public static void sort(int[] arr) {
+        for(int i = 0; i < arr.length; i++)
+        {
+            int currentNum = arr[i];
 
+            int currentIndex = i - 1;
+            while(currentIndex >= 0 && arr[currentIndex] > currentNum)
+            {
+                arr[currentIndex + 1] = arr[currentIndex];
+                currentIndex--;
+            }
+
+            arr[currentIndex + 1] = currentNum;
+        }
     }
 }
--- app/src/main/java/searchsort/MergeSort.java
+++ app/src/main/java/searchsort/MergeSort.java
@@ -6,13 +6,17 @@ public class MergeSort extends Sort {
      */
     @Override
     public void sort(int[] arr) {
-
+        int temp[] = new int[arr.length];
+        mergeSortHelper
     }
 
     /*
      * Merge the two sorted arrays left and right into the array arr.
      */
-    private void merge(int[] arr, int[] left, int[] right) {
-
+    private void merge(int[] arr, int[] left, int[] right, int[] temp) {
+        if(right - left >= 1)
+        {
+            
+        }
     }
 }
