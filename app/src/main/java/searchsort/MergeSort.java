cbf14ec3b7f50df1ec2712f632690f316b4737f5
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-12-08 23:05:47 -0500

    test

--- app/src/main/java/searchsort/MergeSort.java
+++ app/src/main/java/searchsort/MergeSort.java
@@ -6,10 +6,35 @@ public class MergeSort extends Sort {
      */
     @Override
     public void sort(int[] arr) {
-        int temp[] = new int[arr.length];
-        merge(arr, )
+        int[] temp = new int[arr.length];
+
+        mergeSortHelper(arr, temp);
     }
 
+    public void mergeSortHelper(int[] arr, int[] temp)
+    {
+        if(arr.length <= 1)
+        {
+            return;
+        }
+
+        int middle = arr.length / 2;
+
+        int[] left = new int[middle];
+        int[] right = new int[arr.length - middle];
+
+        for (int i = 0; i < middle; i++) {
+            left[i] = arr[i];
+        }
+        
+        for (int i = middle; i < arr.length; i++) {
+            right[i - middle] = arr[i];
+        }
+        mergeSortHelper(left, temp);
+        mergeSortHelper(right, temp);
+
+        merge(arr, left, right, temp);
+    }
     /*
      * Merge the two sorted arrays left and right into the array arr.
      */
@@ -20,7 +45,7 @@ public class MergeSort extends Sort {
 
         while(i < left.length && j < right.length)
         {
-            if(left[i] <= right[i])
+            if(left[i] <= right[j])
             {
                 temp[k] = left[i];
                 k++;
