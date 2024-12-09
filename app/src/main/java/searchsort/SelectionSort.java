3d9c1dee9a7f691dbabf2e608e37c466c1f7328e
scarlettho <159584626+scarlettho@users.noreply.github.com>
2024-12-09 12:57:34 -0500

    updATING FILES

--- app/src/main/java/searchsort/SelectionSort.java
+++ app/src/main/java/searchsort/SelectionSort.java
@@ -5,6 +5,29 @@ public class SelectionSort {
      * Sort the array arr using selection sort algorithm.
      */
     public static void sort(int[] arr) {
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+            int minIndex = findMin(arr, i);
+            swap(arr, i, minIndex);
+        }
+    }
+    private int findMin(int[] arr, int startIndex)
+    {
+        int minIndex = startIndex;
+        for(int i = minIndex + 1; i < arr.length; i++)
+        {
+            if(arr[i] < arr[minIndex])
+            {
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
 
+    private void swap(int[] arr, int x, int y)
+    {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
     }
 }
